bcfc1bae904596897aeca96bb77f22c76048e8de
zamondaz <mgga315@office.khu.ac.kr>
2023-05-19 17:00:58 +0900

    fin

--- coa/coa11_main.cpp
+++ coa/coa11_main.cpp
@@ -8,21 +8,21 @@ void setup() {
     imageMode(CENTER);
     image(backpic, width / 2, height / 2);
 
-    aa = new picture3[9];
-    for (int i = 0; i < 9; i++) {
+    aa = new picture3[6];
+    for (int i = 0; i < 6; i++) {
         aa[i] = new picture3(random(0, width), random(0, height), 80);
     }
 }
 
 void draw() {
     background(backpic);
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 6; i++) {
         aa[i].ship();
         aa[i].actship();
     }
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 6; i++) {
         if (i != 20) {
-            for (int j = i + 1; j < 9; j++) {
+            for (int j = i + 1; j < 6; j++) {
                 aa[i].collide(aa[j]);
             }
         }
